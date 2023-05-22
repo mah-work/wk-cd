0bae6c63b2cf2b06022364ba0d6cd84d3e6c809d
mah-work <132890999+mah-work@users.noreply.github.com>
2023-05-22 11:39:59 +0200

    Updated jsf.js
    Added RSC (replace specific code url)

--- jsf.js
+++ jsf.js
@@ -1,1 +1,7 @@
+/* 
+replace specific code in the URL 
+$("img.photo").each(function(){this.src=this.src.replace("pictures","images")});
 
+// console testing
+console.log($("img.photo").map((i,img)=>img.src).get())
+*/
